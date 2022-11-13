e5174870b1c1be2596612de3eafd539b206af2b4
Qboi123 <qboiwastaken@gmail.com>
2022-11-13 17:34:36 +0100

    Initial commit

---
+++ src/main/java/com/ultreon/mods/motobox/Motobox.java
@@ -0,0 +1,10 @@
+package com.ultreon.mods.motobox;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Motobox implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/ultreon/mods/motobox/client/MotoboxClient.java
@@ -0,0 +1,13 @@
+package com.ultreon.mods.motobox.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class MotoboxClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
